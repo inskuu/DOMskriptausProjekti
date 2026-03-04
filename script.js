c3c37f71d5c4b7beba75d6cad050850ffbccb73b
IinaKarhu <iina.e.karhu@student.laurea.fi>
2026-03-04 22:02:18 +0200

    Korjauksia

--- script.js
+++ script.js
@@ -2,15 +2,22 @@
 const inputLaatikko = document.querySelector("#inputLaatikko");
 const listanSisalto = document.querySelector("#listanSisalto");
 const lisaaNappi = document.querySelector("#lisaaNappi");
+const rivi = document.querySelector(".rivi")
 
 //Lisää uusi <li> painamalla nappia
 lisaaNappi.addEventListener("click" , lisaaHarjoitus);
 function lisaaHarjoitus() {
+    if (inputLaatikko.value === "") {
+        naytaVirhe();
+        return;
+    }
     let li = document.createElement("li");
     li.innerHTML = inputLaatikko.value;
     listanSisalto.appendChild(li);
-    inputLaatikko.value = "";   
+    inputLaatikko.value = "";
+    poistaVirhe();   
 }
+
  //Lisää uusi <li> painamalla enteriä
 inputLaatikko.addEventListener("keydown", painaEnter); 
 function painaEnter(e) {
@@ -18,6 +25,24 @@ if (e.key === "Enter") {
     lisaaHarjoitus()
 }
 }
+
+//Lisää virhe
+function naytaVirhe() {
+        inputLaatikko.placeholder = "Kirjoita harjoitus ennen lisäämistä."
+        inputLaatikko.classList.add("virhe");
+        rivi.classList.add('virhe');
+    } 
+//Poista virhe
+    function poistaVirhe() {
+        inputLaatikko.classList.remove("virhe");
+        rivi.classList.remove('virhe');
+}
+
+inputLaatikko.addEventListener("input", function(e) {
+    if (e.target.value.length > 0) {poistaVirhe();
+}
+});
+
 
 
 //Merkitse valmiiksi tehdyt
